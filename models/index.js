010408a4726fb9dc9c774194b388b6cbb8deb42f
AlokSinghThakur <aloksinghthakurcse@gmail.com>
2023-06-23 12:22:05 +0530

    database connection changed

--- models/index.js
+++ models/index.js
@@ -3,27 +3,31 @@ const { Sequelize } = require('sequelize');
 const userModel = require('./users');
 
 exports.db_config = new Sequelize(
-    'bitespeed',
-    // 'dummy',
-    // 'root',
-    'root',
-    // 'password',
-    'password',
-    {
-        host:  'localhost',
-        // host :'localhost',
-        dialect: 'mysql',
-        operatorsAliases: 0,
-        pool: {
-          max: 5,
-          min: 0,
-          acquire: 30000,
-          idle: 10000
-        },
-        timezone: '+05:30',
-        logging: false,
-      }
-  );
+    'sql12627238',
+    'sql12627238',
+    'ZGrXUV1rzY',
+  
+        // 'wysa',
+        // 'dummy',
+        // 'root',
+        // 'root',
+        // 'password',
+        // 'password',
+        {
+          host:  'sql12.freemysqlhosting.net',
+          // host :'localhost',
+          dialect: 'mysql',
+          operatorsAliases: 0,
+          pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+          },
+          timezone: '+05:30',
+          logging: false,
+        }
+      );
 
   exports.userModel = userModel(exports.db_config);
   
